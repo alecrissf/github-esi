df766c8a3f5ed3ba237e3f73c301e34b44d79c5a
Jonathan Davi Sampaio Faria <jonathandsf_@hotmail.com>
2025-05-15 19:22:07 -0300

    Adiciona docstrings em todas as funções

--- calc.py
+++ calc.py
@@ -1,10 +1,19 @@
 def sub(x, y):
+    """
+    Subtrai y de x.
+    """
     return x - y
 
 
 def sum(x, y):
+    """
+    Soma x e y.
+    """
     return x + y
 
 
 def div(x, y):
+    """
+    Divide x por y retornando None quando y == 0.
+    """
     return x / y if y != 0 else None
